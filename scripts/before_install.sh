7619e93d8c717ede4365654781d8346428eebb3c
Noboru Saito <noborusai@gmail.com>
2019-10-05 10:18:59 +0900

    11.5

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir travis
-git clone --depth=1 -b doc_ja_11 https://github.com/pgsql-jp/jpug-doc.git travis/jpug-doc
+git clone --depth=1 https://github.com/pgsql-jp/jpug-doc.git travis/jpug-doc
 
 mkdir -p ~/.fonts/
 cp scripts/fonts/* ~/.fonts/
